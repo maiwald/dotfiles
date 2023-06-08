b585a668a39a5a6c357bfb7b15ffeaf3870d8220
Luciano Maiwald <mail@luciano-maiwald.de>
2023-06-08 11:55:15 +0200

    nvim: Add projectionist config for ruby & rspec

--- .config/nvim/after/plugin/projectionist.lua
+++ .config/nvim/after/plugin/projectionist.lua
@@ -2,5 +2,9 @@ vim.g.projectionist_heuristics = {
    ["*.go"] = {
      ["*.go"] = { alternate = '{}_test.go', type = 'source' },
      ["*_test.go"] = { alternate = '{}.go', type = 'test' },
+   },
+   ["*.rb"] = {
+     ["*.rb"] = { alternate = 'spec/{}_spec.rb', type = 'source' },
+     ["spec/*_spec.rb"] = { alternate = '{}.rb', type = 'test' },
    }
  }
