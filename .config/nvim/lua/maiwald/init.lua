4a4e9956abd67853dfe71031d3fc8788e53d6f72
Luciano Maiwald <mail@luciano-maiwald.de>
2023-10-02 09:56:00 +0200

    nvim: Move config from init.lua to my module

--- .config/nvim/lua/maiwald/init.lua
+++ .config/nvim/lua/maiwald/init.lua
@@ -1,2 +1,24 @@
+require("maiwald.packer")
 require("maiwald.set")
 require("maiwald.remap")
+
+local function has_value (tab, val)
+    for _, value in ipairs(tab) do
+        if value == val then
+            return true
+        end
+    end
+
+    return false
+end
+
+vim.api.nvim_create_autocmd('BufWritePre', {
+    pattern = "*",
+    callback = function()
+        if not has_value({"markdown", "yaml"}, vim.bo.filetype) then
+            local save = vim.fn.winsaveview()
+            vim.api.nvim_command(":%s/\\s\\+$//e")
+            vim.fn.winrestview(save)
+        end
+    end,
+})
