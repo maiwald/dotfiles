48b4ca14f9226ce6e7d3d754c42c3260b6c6b847
Luciano Maiwald <mail@luciano-maiwald.de>
2024-04-22 10:37:35 +0200

    nvim: Add missing treesitter-context config

---
+++ .config/nvim/after/plugin/treesitter-context.lua
@@ -0,0 +1,5 @@
+require'treesitter-context'.setup{
+  enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+  max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+  multiline_threshold = 5, -- Maximum number of lines to show for a single context
+}
