67b1977268cc1ed1e1aa35d4b521a763c0561ed6
Luciano Maiwald <mail@luciano-maiwald.de>
2026-04-08 12:41:03 +0200

    nvim: Update telescope.nvim

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,7 +1,7 @@
 return {
     {
         'nvim-telescope/telescope.nvim',
-        branch = '0.1.x',
+        version = '0.2.x',
         dependencies = {
             'nvim-lua/plenary.nvim',
             {
